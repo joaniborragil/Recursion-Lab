7dfdd8185c1d1983541e9678eae5cc00c86cc8c4
joaniborragil <joan_gil@avenues-ny.org>
2024-12-16 06:37:58 -0500

    Update fibs.js

--- fibs.js
+++ fibs.js
@@ -4,5 +4,17 @@ function fibonacci(n, memo = []) {
   // If n is 0 or 1, we already know the answer
   if (n === 0) return 0;
   if (n === 1) return 1;
-
+ // Is the number already calculated?
+  if (memo[n] !== undefined) {
+    return memo[n]; // No need to calculate again, in order to return the result
+  }
+ // Store the result in 'memo' so we don't do it again
+  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+  return memo[n];
+}
+// Let's see the first 10 Fibonacci numbers
+console.log("Here are the first 10 Fibonacci numbers:");
+for (let i = 0; i < 10; i++) {
+  console.log(`fib(${i}) = ${fibonacci(i)}`);
+}
   
