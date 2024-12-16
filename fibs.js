e09b4ead929300b64de81f5c332fefe09bba4698
joaniborragil <joan_gil@avenues-ny.org>
2024-12-16 06:35:11 -0500

    Step 1 fibs.js

--- fibs.js
+++ fibs.js
@@ -1,1 +1,8 @@
+// Let's calculate Fibonacci numbers. We'll use memorization.
+// In order to avoid repeating work we've already done.
+function fibonacci(n, memo = []) {
+  // If n is 0 or 1, we already know the answer
+  if (n === 0) return 0;
+  if (n === 1) return 1;
 
+  
